e436f82fb8bd9133d884e41305102454e4fcc168
YWS01 <yara.wehmuthsoler@sainsburys.co.uk>
2026-01-26 14:23:15 +0000

    Initial commit: add project and .gitattributes

---
+++ otj-logger/src/main/java/com/hackathon/otj_logger/OtjLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.otj_logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtjLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtjLoggerApplication.class, args);
+	}
+
+}
---
+++ otj-logger/src/test/java/com/hackathon/otj_logger/OtjLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.otj_logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtjLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
